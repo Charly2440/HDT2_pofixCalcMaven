62d97d49789f912f92d4c498af2eb32434f15fa6
Carlos Villagran <cjvillagranmata@gmial.com>
2024-01-30 10:47:45 -0600

    Clases generadas en estructura maven

---
+++ src/main/java/org/example/PosfixCalc.java
@@ -0,0 +1,5 @@
+package org.example;
+
+public interface PosfixCalc {
+    public Integer calc(String problem);
+}
---
+++ src/main/java/org/example/UvgStack.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public interface UvgStack<T> {
+    public T top();
+    public void push(T obj);
+    public T pop();
+    public boolean isEmpty();
+}
